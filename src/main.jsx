3f7cdb667ac824af2d640e843b30de3392178f24
Giedrius K <giedkon@gmail.com>
2023-03-15 00:22:20 +0200

    Restructured folders

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,74 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { useState } from 'react';
+
+// CSS
+import './assets/css/index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import "./index.css";
+
+import Root from './routes/root';
+import ErrorPage from './error_pages/404';
+import Dashboard from './pages/dashboard';
+import Computer from './pages/computer';
+import ComputerList from './pages/computerList';
+
+
+const defaultGlobalState = {
+  open: false,
+};
+const globalStateContext = React.createContext(defaultGlobalState);
+const dispatchStateContext = React.createContext(undefined);
+
+const GlobalStateProvider = ({ children }) => {
+  const [state, dispatch] = React.useReducer(
+    (state, newValue) => ({ ...state, ...newValue }),
+    defaultGlobalState
+  );
+  return (
+    <globalStateContext.Provider value={state}>
+      <dispatchStateContext.Provider value={dispatch}>
+        {children}
+      </dispatchStateContext.Provider>
+    </globalStateContext.Provider>
+  );
+};
+
+export const useGlobalState = () => [
+  React.useContext(globalStateContext),
+  React.useContext(dispatchStateContext)
+];
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Dashboard />,
+      },
+      {
+        path: "/computers",
+        element: <ComputerList />,
+      },
+      {
+        path: "computer/:computerId",
+        element: <Computer />,
+      },
+    ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+    <GlobalStateProvider>
+      <RouterProvider router={router} />
+    </GlobalStateProvider>
+  </React.StrictMode>,
+)
